a5131152393cfd97a8f2f0d6b82d967295a53c7f
Reiner Optiz <90814876+reinerism@users.noreply.github.com>
2023-02-22 19:41:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ from db import get
 #creatin an instance of flask
 app = Flask(__name__)
 
-db_user = os.environ.get('CLOUD_SQL_USERNAME'),
-db_password = os.environ.get('CLOUD_SQL_PASSWORD'),
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME'),
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME'),
-
-
 @app.route('/', methods=['GET'])
 def get_songs():
     return get()
