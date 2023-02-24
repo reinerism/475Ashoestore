1b9c46bd90837c06d9f616eaf621c8a7a4a7434a
reinerism <ropitz@uw.edu>
2023-02-24 15:14:37 -0800

    Updating main.py

--- main.py
+++ main.py
@@ -25,15 +25,13 @@ db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def main():
+def get_db():
     # When deployed to App Engine, the `GAE_ENV` environment variable will be
     # set to `standard`
     if os.environ.get('GAE_ENV') == 'standard':
         # If deployed, use the local socket interface for accessing Cloud SQL
         unix_socket = '/cloudsql/{}'.format(db_connection_name)
-        cnx = pymysql.connect(user=db_user, password=db_password,
+        return pymysql.connect(user=db_user, password=db_password,
                               unix_socket=unix_socket, db=db_name)
     else:
         # If running locally, use the TCP connections instead
@@ -41,18 +39,25 @@ def main():
         # so that your application can use 127.0.0.1:3306 to connect to your
         # Cloud SQL instance
         host = '127.0.0.1'
-        cnx = pymysql.connect(user=db_user, password=db_password,
+        return pymysql.connect(user=db_user, password=db_password,
                               host=host, db=db_name)
 
+def get_query(cnx):
     with cnx.cursor() as cursor:
         cursor.execute('YOUR QUERY GOES HERE;')
         result = cursor.fetchall()
         current_msg = result[0][0]
-    cnx.close()
+    return current_msg
+
+@app.route('/')
+def main():
+    cnx = get_db
+    current_msg = get_query(cnx)
 
     return str(current_msg)
 # [END gae_python37_cloudsql_mysql]
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    app.run()
+    # app.run(host='127.0.0.1', port=8080, debug=True)
